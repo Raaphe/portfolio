6ef27db6e0a12bbfaf8792b08de44544a3037b3a
Raaphe <120033739+Raaphe@users.noreply.github.com>
2022-12-16 20:19:57 -0500

    day 6 in aoc
    here I learned how to use comprehensive sets and put them to use. second part of the 6th day.

--- aoc_2022/day_6#2.py
+++ aoc_2022/day_6#2.py
@@ -1,11 +1,16 @@
 # Open file + variable
 data = open('transmission.dat', 'r').read()
-transm = ''
-char_li = []
 
 # puts each character in the string in a list I can iterate through
-for char in data:
-    char_li.append(char)
+char_li = [char for char in data]
 
-print(char_li)
+# main loop, iterates through each string
+for ii in range(len(data)-13):
+   
+    # puts into a set strings, 14 at a time
+    char_set = {char for char in char_li[ii:ii+14]}
 
+    # if the set is 14 (sets can only have on of the same value), the code breaks after having printed the answer
+    if len(char_set) == 14:
+        print(ii+14)
+        break;
