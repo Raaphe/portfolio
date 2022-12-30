3e7436af61c4442abd862dac1ddc62e0d9b6b67c
Raaphe <120033739+Raaphe@users.noreply.github.com>
2022-12-30 18:23:38 -0500

    day 9 part 2
    DONE

--- aoc_2022/day_9#2.py
+++ aoc_2022/day_9#2.py
@@ -63,7 +63,28 @@ def next_tail_pos(headx, heady, tailx, taily):
     # Right
     elif tail_pos == (headx - 2, heady):
         return (headx - 1, heady);
+
+    # Part 2 new movement cases
+
+
+    # === Diagonal cases ===
+
+
+    # Right
+    elif tail_pos == (headx - 2, heady - 2):
+        return (headx - 1,heady - 1)
         
+    # Left 
+    elif tail_pos == (headx + 2, heady - 2):
+        return (headx + 1,heady - 1)
+    
+    # Down Right
+    elif tail_pos == (headx - 2, heady + 2):
+        return (headx - 1,heady + 1)
+
+    # Down Left
+    elif tail_pos == (headx + 2, heady + 2):
+        return (headx + 1,heady + 1)
 
 # Variables
 head_pos = (0,0)
@@ -103,30 +124,43 @@ with open('day9_input.dat','r') as file:
             li = list(rope['knot0'])
             li.append((head_pos))
             rope.update({'knot0':li})
-    
-# for each knot0 movement, in one loop, check if the other knots
-# change positions using the movement function
 
 
-# each knot here is represented by a loop
 
-for ii in range(len(rope['knot0'])) :
 
-   for jj in range(9):
-       
-       
-      headx, heady = rope[f'knot{str(jj)}'][ii]    
-      tailx, taily = rope[f'knot{str(jj+1)}'][-1]
+# Main umbrella loop for all the rope knot position changes
+for ii in rope['knot0'] :
+
+    # kicks off the head of the rope before finding the other 7 knots
+    headx, heady = ii
+    tailx, taily = rope['knot1'][-1]
          
-      new_tail_pos = next_tail_pos(headx, heady, tailx, taily)
+    new_tail_pos = next_tail_pos(headx, heady, tailx, taily)
 
-      if new_tail_pos == None:
-          continue;
-      else:
-          li = list(rope['knot1'])
-          li.append(new_tail_pos)
-          rope.update({'knot1': li})
+    if new_tail_pos == None:
+        continue;
+    else:
+        li = list(rope['knot1'])
+        li.append(new_tail_pos)
+        rope.update({'knot1': li})
+        
+
+    # loop here finds and updates the other 7 knots each iteration
+    for jj in range(1,9):
+       
+       
+        headx, heady = rope[f'knot{str(jj)}'][-1]    
+        tailx, taily = rope[f'knot{str(jj+1)}'][-1]
+         
+        new_tail_pos = next_tail_pos(headx, heady, tailx, taily)
+
+        if new_tail_pos == None:
+            continue;
+        else:
+            li = list(rope[f'knot{str(jj+1)}'])
+            li.append(new_tail_pos)
+            rope.update({f'knot{str(jj+1)}': li})
         
     
-#on paper this code works
+#prints answer
 print(len(set(rope['knot9'])))
